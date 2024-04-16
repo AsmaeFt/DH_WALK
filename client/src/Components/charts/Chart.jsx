48833aa68ccaff19bf7c6053e7e7f606c14f7476
AsmaeFt <kimasmac@gmail.com>
2024-04-16 17:05:27 +0100

    new

--- client/src/Components/charts/Chart.jsx
+++ client/src/Components/charts/Chart.jsx
@@ -31,7 +31,9 @@ const LineChartComponent = () => {
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
-        tension: 0.4
+        tension: 0.4,
+        fill:true
+
       },
       {
         label: 'Dataset 2',
